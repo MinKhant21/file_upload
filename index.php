3a4b10014ef3cab8444b7d1b5d1abd4db914cfc1
KaungMinKhant <kaungminkhant6889@gmail.com>
2022-06-03 02:21:08 +0630

    multi_file_upload

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 
-echo "<pre>";
-print_r($_FILES);
-    $file = $_FILES;
-    $limit_size = 1024 * 1024 * 2; // 2mb
-    if($limit_size < $file['image']['size']){
-         echo "limited below 2mb";
-    }else{
-        $tmp_name = $file['image']['tmp_name'];
-        $file_name = uniqid().$file['image']['name'];
-        move_uploaded_file($tmp_name , './image/'.$file_name);
-        echo "success";
+    echo "<pre>";
+    print_r($_FILES);
+
+    if(isset($_POST['btnupload'])){
+        $file = $_FILES;
+        foreach($file['image']['name'] as $k=>$value){
+            $tmp_name = $file['image']['tmp_name'][$k];
+            $file_name = uniqid().$value;
+            move_uploaded_file($tmp_name,'./image/'.$file_name);
+        }
     }
 
 ?>
 
 <form action="" enctype="multipart/form-data" method="POST">
 
-<input type="file" name="image">
-<input type="submit" name="btnupload" value="upload">
+<input type="file" name="image[]" multiple id="">
+<input type="submit" name="btnupload" value="Upload">
 
 </form>
